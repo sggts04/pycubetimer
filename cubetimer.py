ee30542b03ab2e543b573918633d67ea6b175376
Shreyas Gupta <technology.shreyas@gmail.com>
2018-08-11 17:25:24 +0530

    Add files via upload

--- cubetimer.py
+++ cubetimer.py
@@ -19,7 +19,7 @@ def watch() :
         if keyboard.is_pressed('space'):
             freeze()
 def freeze():
-    global b, timed, count, times_list
+    global b, timed, count, times_list, p, avg5, avg5_flo, times_list_5_flo, times_list_5, min_5, max_5, min_c, max_c
     b=b+1
     scramble.config(text = scrambler333.get_WCA_scramble())
     times_file = open('times.txt', 'a+')
@@ -47,6 +47,33 @@ def freeze():
             i = i + 1
     times_count.config(text='No. of Solves: ' + str(count))
     saved_times.config(text='Last 12 Solves: ' + saved_times_str)
+    avg5 = ''
+    avg5_flo = 0
+    min_c = 0
+    max_c = 0
+    if (count > 5):
+        times_list_5 = times_list[count - 5:count]
+        times_list_5_flo = [0, 1, 2, 3, 4]
+        p = 0
+        while (p < 5):
+            times_list_5_flo[p] = float(times_list_5[p].strip())
+            p = p + 1
+        min_5 = min(times_list_5_flo)
+        max_5 = max(times_list_5_flo)
+        p = 0
+        while (p < 5):
+            if (times_list_5_flo[p] == max_5 and max_c == 0):
+                p = p + 1
+                max_c = 1
+                continue
+            if (times_list_5_flo[p] == min_5 and min_c == 0):
+                p = p + 1
+                min_c = 1
+                continue
+            avg5_flo = avg5_flo + (times_list_5_flo[p] / 3)
+            p = p + 1
+        avg5 = '%.2f' % (avg5_flo)
+    ao5.config(text = 'Average of 5: ' + avg5)
     root.update()
 
 root = Tk()
@@ -86,5 +113,33 @@ stop.grid(row=2, column=1, pady=15, padx=10)
 times_count.grid(row=3, columnspan=2, pady =10, padx = 5)
 saved_times.grid(row=4, columnspan=2, pady=10, padx = 10)
 scramble.grid(row=0, columnspan=2, pady=15, padx = 5)
+avg5 = ''
+avg5_flo = 0
+max_c=0
+min_c=0
+if(count>=5):
+    times_list_5 = times_list[count-5:count]
+    times_list_5_flo = [0,1,2,3,4]
+    p=0
+    while(p<5):
+        times_list_5_flo[p] = float(times_list_5[p].strip())
+        p=p+1
+    min_5 = min(times_list_5_flo)
+    max_5 = max(times_list_5_flo)
+    p=0
+    while(p<5):
+      if(times_list_5_flo[p] == max_5 and max_c == 0):
+         p=p+1
+         max_c = 1
+         continue
+      if(times_list_5_flo[p] == min_5 and min_c == 0):
+          p = p + 1
+          min_c = 1
+          continue
+      avg5_flo = avg5_flo + (times_list_5_flo[p]/3)
+      p = p+1
+    avg5 = '%.2f' % (avg5_flo)
+ao5 = Label(root, text = 'Average of 5: ' + avg5, font=20, background='#f6e58d', fg = 'black')
+ao5.grid(row=5, columnspan = 2, pady=5, padx=10)
 root.title('Cube Timer')
 root.mainloop()
